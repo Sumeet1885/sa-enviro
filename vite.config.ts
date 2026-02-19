929705bc597d49cbe6ea80ec135e4a3d274cad45
ParasBharde <66599050+ParasBharde@users.noreply.github.com>
2026-02-19 11:16:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: "/", // Ensures assets are referenced correctly relative to the root
-  },
+  base: "/",
   plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
